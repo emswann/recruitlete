01818953f433e777159a7109ebd1d8132d200d03
Elaina Swann <elaina@odecisions.com>
2018-05-01 18:49:24 -0400

    Comments out unused Nav component and removes material-ui from server json package.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Nav from "./components/Nav"
+// import Nav from "./components/Nav"
 import Slideshow from "./components/Slideshow"
 
 const App = () => (

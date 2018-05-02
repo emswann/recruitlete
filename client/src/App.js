639d0291c9872b9ad722cdacc0e96e7b503232b7
kahonen <kristin.ahonen@gmail.com>
2018-05-01 22:59:56 -0400

    Add navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-//import Nav from "./components/Nav"
+import Nav from "./components/Nav"
 import Slideshow from "./components/Slideshow"
 
 const App = () => (
-<div>
-
+<div >
+  <Nav/>
+  <div style={{ paddingLeft: 100, paddingRight: 100 }}>
   <Slideshow/>
+  </div>
   </div>
   // <Router>
   //   <div>

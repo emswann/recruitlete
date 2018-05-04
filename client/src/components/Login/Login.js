65c2d95fc8e23873868a018513baad26a7c83cdc
kahonen <kristin.ahonen@gmail.com>
2018-05-04 09:12:26 -0400

    Add Bootstrap cdn

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Button, Input} from 'mdbreact';
-
-
+import React from "react";
+import PropTypes from "prop-types";
+import { Button, Input } from "mdbreact";
 
 class Login extends React.Component {
-
-    render() {
-
+  render() {
     return (
-        <form>
-            <p className="h5 text-center mb-4">Sign in</p>
-            <Input label="Type your email" icon="envelope" group type="email" validate error="wrong" success="right"/>
-            <Input label="Type your password" icon="lock" group type="password" validate/>
-            <div className="text-center">
-                <Button block color="primary">Login</Button>
-                <Button block color="secondary">Register</Button>
-            </div>
-        </form>
+      <form>
+        <p className="h5 text-center justify-center mb-4">Sign in</p>
+        <div className="radio">
+          <label>
+            <input type="radio" value="athlete" checked={true} />
+            Athlete
+          </label>
+        </div>
+        <div className="radio">
+          <label>
+            <input type="radio" value="coach" />
+            Coach
+          </label>
+        </div>
+        <br />
+        <Input
+          label="E-mail"
+          icon="envelope"
+          group type="email"
+          validate error="wrong"
+          success="right"
+        />
+        <Input
+          label="Password"
+          icon="lock"
+          group type="password"
+          validate
+        />
+        <div className="text-center">
+          <Button block color="danger">
+            Login
+          </Button>
+          <Button block color="secondary">
+            Register
+          </Button>
+        </div>
+      </form>
     );
-}
-
+  }
 }
 
 export default Login;
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Nav from "../../components/Nav";
 import Slideshow from "../../components/Slideshow";
-import Card from "../../components/Card"
-import Login from "../../components/Login"
+import Card from "../../components/Card";
+import Login from "../../components/Login";
 //import Newsfeed from '../../components/NewsFeed';
 
 const Home = () => (
@@ -13,18 +13,16 @@ const Home = () => (
     <div className="row">
       <div className="col">
         <Slideshow />
-<div className="row">
-<div className="col-md-6">
-            <Card style={{padding:20}}>
+        <div className="row">
+          <div className="col-md-6">
+            <Card style={{ padding: 20 }}>
               <Login />
             </Card>
-</div>
-</div>
-<div className="col-md-6">
-            <Card>
-              {/* <Newsfeed /> */}
-            </Card>
-</div>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <Card>{/* <Newsfeed /> */}</Card>
+        </div>
       </div>
     </div>
   </div>

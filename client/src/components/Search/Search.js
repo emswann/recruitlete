198cff35541fb974e9ead5651e84589becd3da88
Elaina Swann <elaina@odecisions.com>
2018-05-09 19:06:56 -0400

    More merge changes.

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -2,63 +2,62 @@ import React from "react";
 import { Button } from "mdbreact";
 
 const Search = props => (
-  <div className="container" style={{ overflow: "scroll", height: 420 }}>
+  <div className="container" style={{ overflow: "scroll", height: 420, border: "#000000" }}>
     {props.searchSchools.length ? (
       <div className="row">
         <div className="col-md-12">
-          <table className="table table-hover table-bordered">
-            <thead className="header-fixed">
+          <table className="table table-hover table-bordered header-fixed">
+            <thead>
               <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Conference</th>
-                <th scope="col">Division</th>
-                <th scope="col">State</th>
-                <th scope="col">Region</th>
-                <th scope="col">Save</th>
+                <th>Name</th>
+                <th>Conference</th>
+                <th>Division</th>
+                <th>State</th>
+                <th>Region</th>
+                <th>Save</th>
               </tr>
             </thead>
-            <tbody>
-              {props.searchSchools.map((school, index) => (
-                <tr key={index}>
-                  <th>
-                    <a target="_blank" href={school.collegeLink}>
-                      {school.name}
-                    </a>
-                  </th>
-                  <th>
-                    <a target="_blank" href={school.conferenceLink}>
-                      {school.conference}
-                    </a>
-                  </th>
-                  <th>{school.division}</th>
-                  <th>{school.state}</th>
-                  <th>{school.region}</th>
-                  <th>
-                    <Button
-                      block
-                      color="danger"
-                      type="button"
-                      name="action"
-                      value="saveBtn"
-                      disabled={school.saved} 
-                      onClick={() =>
-                        props.handleSaveSchool({
-                          name: school.name,
-                          conference: school.conference,
-                          division: school.division,
-                          state: school.state,
-                          region: school.region,
-                          collegeLink: school.collegeLink,
-                          conferenceLink: school.conferenceLink
-                        })
-                      }
-                    >
-                      Save
-                    </Button>
-                  </th>
-                </tr>
-              ))}
-            </tbody>
+            {props.searchSchools.map((school, index) => (
+              <tbody key={index}>
+                  <tr>
+                    <th>
+                      <a target="_blank" href={school.collegeLink}>
+                        {school.name}
+                      </a>
+                    </th>
+                    <th>
+                      <a target="_blank" href={school.conferenceLink}>
+                        {school.conference}
+                      </a>
+                    </th>
+                    <th>{school.division}</th>
+                    <th>{school.state}</th>
+                    <th>{school.region}</th>
+                    <th>
+                      <Button
+                        block
+                        color="danger"
+                        type="button"
+                        name="action"
+                        value="saveBtn"
+                        onClick={() =>
+                          props.handleSaveSchool({
+                            name: school.name,
+                            conference: school.conference,
+                            division: school.division,
+                            state: school.state,
+                            region: school.region,
+                            collegeLink: school.collegeLink,
+                            conferenceLink: school.conferenceLink
+                          })
+                        }
+                      >
+                        Save
+                      </Button>
+                    </th>
+                  </tr> 
+              </tbody>
+            ))}
           </table>
         </div>
       </div>

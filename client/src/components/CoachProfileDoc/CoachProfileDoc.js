38ebd721dae603ff1459440561e7d23ab735eebe
Elaina Swann <elaina@odecisions.com>
2018-05-15 11:53:16 -0400

    Adds coach profile document.

--- client/src/components/CoachProfileDoc/CoachProfileDoc.js
+++ client/src/components/CoachProfileDoc/CoachProfileDoc.js
@@ -1,9 +1,53 @@
 import React from "react";
+import { Panel } from "react-bootstrap";
 
 const CoachProfileDoc = props => (
-  <div className="container">
-    <h1>Coach Profile Doc</h1>
-  </div>
+  <Panel>
+    <Panel.Heading className="mb-3">
+      <Panel.Title>
+        <h4>
+          {props.user.name.first} {props.user.name.middle} {props.user.name.last}
+        </h4>
+      </Panel.Title>
+    </Panel.Heading>
+    <Panel.Body>
+      <div className="row">
+        <div className="col-md-12">
+          <h6>
+            {props.user.position}
+          </h6>
+          <h6>
+            {props.user.address.street1}
+          </h6>
+          {props.user.address.street2 && (
+            <h6>
+              {props.user.address.street2}
+            </h6>              
+          )}
+          {props.user.address.street2 && (
+            <h6>
+              {props.user.address.city}, {props.user.address.state} {props.user.address.zip}
+            </h6>              
+          )}
+          <br />
+          <h6>
+            {props.user.contact.phone}
+          </h6>
+          <h6>
+            {props.user.email}
+          </h6>
+          <h6>
+            {props.user.contact.url}
+          </h6>
+          <br />
+          <h6>
+            {props.user.accomplishments}
+          </h6>
+        </div>
+      </div>
+    </Panel.Body>
+  </Panel>
 );
 
 export default CoachProfileDoc;
+
